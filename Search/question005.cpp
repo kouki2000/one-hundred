2306cc052cb289c8d19dff1c3fefbc7dbbaba5a1
kouki2000 <kouki-tus@outlook.jp>
2021-01-17 21:20:50 +0900

    question5

--- Search/question005.cpp
+++ Search/question005.cpp
@@ -7,5 +7,23 @@ int INF = 1001001001;
 
 int main()
 {
+    int a, b, c, x, y;
+    cin >> a >> b >> c >> x >> y;
+    int result = 0;
+
+    if (a + b < c * 2)
+    {
+        result = a * x + b * y;
+    }
+    else
+    {
+        int l = min(x, y);
+        int h = max(x, y);
+        int ans1 = l * c * 2 + (x - l) * a + (y - l) * b;
+        int ans2 = h * c * 2;
+        result = min(ans1, ans2);
+    }
+
+    cout << result << endl;
     return 0;
 }
