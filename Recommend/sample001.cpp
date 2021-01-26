34143639c4e3245096897f840c755c34ce5ee0cf
kouki2000 <kouki-tus@outlook.jp>
2021-01-26 20:27:13 +0900

    001

--- Recommend/sample001.cpp
+++ Recommend/sample001.cpp
@@ -7,5 +7,29 @@ int INF = 1001001001;
 
 int main()
 {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> val;
+    rep(i, n)
+    {
+        cin >> a[i];
+    }
+
+    rep(i, n)
+    {
+        int cnt = 0;
+        while (a[i] % 2 == 0)
+        {
+            a[i] /= 2;
+            cnt++;
+        }
+        val.push_back(cnt);
+    }
+
+    sort(val.begin(), val.end());
+
+    cout << val[0] << endl;
+
     return 0;
 }
